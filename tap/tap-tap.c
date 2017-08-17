4e2c81f83cce668b1a073791e96c8a93b7603aa4
mum-chen <542177535@qq.com>
2017-08-17 22:32:00 +0800

    feat(tap-tap): send to another

--- tap/tap-tap.c
+++ tap/tap-tap.c
@@ -6,10 +6,48 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+#include <sys/epoll.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
+
+#define DEBUG_ON	0
+#define BUFFER_SIZE	4096
+
+#define name_dst "tap_dst"
+#define name_src "tap_src"
+#define ip_dst   "192.168.200.1"
+#define ip_src   "192.168.100.1"
+static int tfdset[2]; /* tap fd set */
+
+#if (defined(DEBUG_ON) && DEBUG_ON)
+enum PORT_TYPE{
+	PORT_SRC = 1,
+	PORT_DST = 2,
+	PORT_UNKNOWN = 3,
+};
+
+static int port_type(const int fd)
+{
+	if (fd == tfdset[0])
+		return PORT_SRC;
+	else if (fd == tfdset[1])
+		return PORT_DST;
+	else
+		return PORT_UNKNOWN;
+}
+#endif
+
+struct tap_device {
+	int fd;
+
+	char ip[16];
+	char name[IFNAMSIZ];
+
+	int length;
+	struct epoll_event event;
+};
 
 /*
  * dev:		char ifname[IFNAMSIZ]
@@ -63,21 +101,92 @@ fail:
 	return -1;
 }
 
+static int tap_init(struct tap_device *tap, char* ethname, char *ip)
+{
+	tap->fd = tun_alloc(ethname, IFF_TAP, ip);
+	if (tap->fd < 0)
+		error(tap->fd, errno, "error in tap");
+
+	tap->event.events = EPOLLIN;
+	tap->event.data.fd = tap->fd;
+	return 0;
+}
+
+static void register_fd(const int src_fd, const int dst_fd)
+{
+	printf("src %d, dst %d\n", src_fd, dst_fd);
+	tfdset[0] = src_fd;
+	tfdset[1] = dst_fd;
+}
+
+static int another_tfd(const int fd)
+{
+	if (fd == tfdset[0])
+		return tfdset[1];
+	else if (fd == tfdset[1])
+		return tfdset[0];
+	else
+		return -1;
+}
+
+static inline void sendto_another(const int fd)
+{
+	int len, another;
+	unsigned char buff[BUFFER_SIZE];
+	len = read(fd, buff, sizeof(buff));
+
+	another = another_tfd(fd);
+	len = write(another, buff, len);
+
+#if (defined(DEBUG_ON) && DEBUG_ON)
+	switch (port_type(fd)) {
+	case PORT_SRC:
+		printf("receive %s:%d, len %d;\n", "tap_src", fd, len);
+		break;
+	case PORT_DST:
+		printf("receive %s:%d, len %d;\n", "tap_dst", fd, len);
+		break;
+	default:
+		printf("receive %s:%d, len %d;\n", "unknown", fd, len);
+		break;
+	}
+#endif
+}
+
 int main(void)
 {
-	char name_dst[IFNAMSIZ] = "tap_dst";
-	char name_src[IFNAMSIZ] = "tap_src";
-	char *ip_dst = "192.168.200.1";
-	char *ip_src = "192.168.100.1";
+	int epollfd, nfds, n;
+	struct tap_device src, dst;
+	struct epoll_event events[2];
 
-	int tap_dst = tun_alloc(name_dst, IFF_TAP, ip_dst);
-	if (tap_dst < 0)
-		error(tap_dst, errno, "error in tap_dst");
+	tap_init(&src, name_dst, ip_dst);
+	tap_init(&dst, name_src, ip_src);
+	register_fd(src.fd, dst.fd);
 
-	int tap_src = tun_alloc(name_src, IFF_TAP, ip_src);
-	if (tap_src < 0)
-		error(tap_src, errno, "error in tap_src");
+	epollfd = epoll_create(2);
+	if (epollfd == -1)
+		goto fail;
 
-	while (1) { }
+
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, src.fd, &src.event) == -1)
+		goto fail;
+
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, dst.fd, &dst.event) == -1)
+		goto fail;
+
+	printf("tap-tap start\n");
+	while (1) {
+		nfds = epoll_wait(epollfd, events, 2, -1);
+		if (nfds == -1)
+			goto fail;
+
+		for (n = 0; n < nfds; n++) {
+			sendto_another(events[n].data.fd);
+		}
+	}
+fail:
+	close(dst.fd);
+	close(src.fd);
+	error(-1, errno, "error");
 	return 0;
 }
