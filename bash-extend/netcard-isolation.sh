3a571d7d2dec20396adff4a7d12996e2a9fb7e4c
Yichen Lin <linyichen@credosemi.com.cn>
2018-01-30 18:09:22 +0800

    feat(netcard-isolaltion): privileges check
    - Support running command directly by set ${EXEC} with "".
    - Support more example
    - Modify iperf example

--- bash-extend/netcard-isolation.sh
+++ bash-extend/netcard-isolation.sh
@@ -9,6 +9,18 @@ IP_GROUP="192.168.50.0/24"
 IP_FULL["${SERVER}"]=${IP_GROUP/.0/.1}
 IP_FULL["${CLIENT}"]=${IP_GROUP/.0/.2}
 
+# EXEC =:: "echo" | ""
+EXEC="echo"
+
+if [ ${EXEC} != "echo" ]; then
+	echo "Superuser privileges are required to run this script."
+	sudo echo "sudo privileges"
+	if [ $? -ne 0 ]; then
+		echo "Permission deny"
+		exit 1
+	fi
+fi
+
 IP["${SERVER}"]=${IP_FULL[${SERVER}]%%/*}
 IP["${CLIENT}"]=${IP_FULL[${CLIENT}]%%/*}
 
@@ -20,16 +32,16 @@ ns_if_up()
 {
 	# $1 := ${SERVER} | ${CLIENT}
 	echo init ${NS[$1]} 
-	echo ip netns add ${NS["$1"]}
-	echo ip link set ${IF[$1]} netns ${NS[$1]}
-	echo ip netns exec ${NS[$1]} ip addr add dev ${IF[$1]} ${IP_FULL[$1]}
-	echo ip netns exec ${NS[$1]} ip link set dev ${IF[$1]} up
+	${EXEC} sudo ip netns add ${NS["$1"]}
+	${EXEC} sudo ip link set ${IF[$1]} netns ${NS[$1]}
+	${EXEC} sudo ip netns exec ${NS[$1]} ip addr add dev ${IF[$1]} ${IP_FULL[$1]}
+	${EXEC} sudo ip netns exec ${NS[$1]} ip link set dev ${IF[$1]} up
 }
 
 ns_if_down()
 {
 	echo destroy ${NS[$1]} 
-	echo ip netns delete ${NS["$1"]}
+	${EXEC} sudo ip netns delete ${NS["$1"]}
 }
 
 help()
@@ -48,8 +60,13 @@ example()
 {
 	echo '-------------------------'
 	echo "iperf example"
-	echo "sudo ip netns exec ${NS[${SERVER}]} iperf -s -B ${IP[${SERVER}]}"
-	echo "sudo ip netns exec ${NS[${CLIENT}]} iperf -c ${IP[${SERVER}]} -B ${IP[${CLIENT}]}"
+	echo "sudo ip netns exec ${NS[${SERVER}]} iperf -s -i 1"
+	echo "sudo ip netns exec ${NS[${CLIENT}]} iperf -c ${IP[${SERVER}]}"
+	echo "sudo ip netns exec ${NS[${CLIENT}]} iperf -s -i 1"
+	echo "sudo ip netns exec ${NS[${SERVER}]} iperf -c ${IP[${CLIENT}]}"
+
+	echo "sudo ip netns exec ${NS[${CLIENT}]} wireshark"
+	echo "sudo ip netns exec ${NS[${SERVER}]} wireshark"
 }
 
 case $@ in
